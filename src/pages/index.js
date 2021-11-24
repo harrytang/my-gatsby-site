f37ac0ec328575710111f824b7ae7013b11c2e35
harrytang <harry@powerkernel.com>
2021-11-24 14:52:41 +0200

    chore: install plugins

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,15 +4,18 @@
  * @copyright Copyright (c) 2021 Power Kernel
  */
 
-// Step 1: Import React
 import * as React from "react";
+import { StaticImage } from "gatsby-plugin-image";
 import Layout from "../components/layout";
 
-// Step 2: Define your component
 const IndexPage = () => {
   return (
     <Layout pageTitle="Home Page">
       <p>I'm making this by following the Gatsby Tutorial.</p>
+      <StaticImage
+        alt="Clifford, a reddish-brown pitbull, posing on a couch and looking stoically at the camera"
+        src="https://pbs.twimg.com/media/E1oMV3QVgAIr1NT?format=jpg&name=large"
+      />
     </Layout>
   );
 };
