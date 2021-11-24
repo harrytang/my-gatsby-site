728a4787e5a0c7f9045a68d9e0cddbc1eaee1868
harrytang <harry@powerkernel.com>
2021-11-24 14:41:39 +0200

    feat: finished Gatsby tutorial part 1 & 2

---
+++ src/components/layout.js
@@ -0,0 +1,43 @@
+/**
+ * @author Harry Tang <harry@powerkernel.com>
+ * @link https://powerkernel.com
+ * @copyright Copyright (c) 2021 Power Kernel
+ */
+
+import * as React from "react";
+import { Link } from "gatsby";
+import {
+  container,
+  heading,
+  navLinks,
+  navLinkItem,
+  navLinkText,
+} from "./layout.module.css";
+
+const Layout = ({ pageTitle, children }) => {
+  return (
+    <div className={container}>
+      <title>{pageTitle}</title>
+      <nav>
+        <ul className={navLinks}>
+          <li className={navLinkItem}>
+            <Link to="/" className={navLinkItem}>
+              Home
+            </Link>
+          </li>
+          <li className={navLinkItem}>
+            <Link to="/about" className={navLinkItem}>
+              About
+            </Link>
+          </li>
+        </ul>
+      </nav>
+      <main>
+        <h1 className={heading}>{pageTitle}</h1>
+        {children}
+      </main>
+    </div>
+  );
+};
+
+export default Layout;
